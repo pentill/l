f3d816502177af37a2a6c11d3be870bc86689933
pentill <76242873+pentill@users.noreply.github.com>
2020-12-24 16:06:30 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,72 @@
+const help = (prefix) => {
+	return `*[BOT] Bentol*
+
+*Sticker Commands*
+╔⊱ *${prefix}sticker*
+╠⊱ *${prefix}tsticker*
+╚⊱ *${prefix}toimg*
+
+*Education*
+╔⊱ *${prefix}brainly* [query]
+╠⊱ *${prefix}kbbi* [query]
+╠⊱ *${prefix}wiki* [query] Wikipedia Indonesia
+╚⊱ *${prefix}wikien* [query] Wikipedia English
+
+*Information*
+╔⊱ *${prefix}infocuaca* [daerah]
+╠⊱ *${prefix}infoig* [@username]
+╚⊱ *${prefix}jadwaltv* [channel]
+
+*Primbon*
+╔⊱ *${prefix}artinama* [nama]
+╠⊱ *${prefix}cekjodoh* [nama] [pasangan]
+╚⊱ *${prefix}zodiak* [nama tgl-bln-thn]
+
+*Creator Commands*
+╔⊱ *${prefix}phub* [teks1 teks2]
+╠⊱ *${prefix}glitch* [teks1 teks2]
+╠⊱ *${prefix}marvel* [teks1 teks2] //error//
+╠⊱ *${prefix}nulis* [teks]
+╠⊱ *${prefix}tahta* [teks]
+╠⊱ *${prefix}qrcode* [teks]
+╠⊱ *${prefix}bpink* [teks]
+╚⊱ *${prefix}party* [teks]
+
+*Fun Commands*
+╔⊱ *${prefix}s* [teks] SimSimi
+╠⊱ *${prefix}gtts* [cc] [teks]
+╠⊱ *${prefix}hilih* [teks]
+╠⊱ *${prefix}meme*
+╠⊱ *${prefix}memeindo*
+╚⊱ *${prefix}nekonime*
+
+*Grup Commands*
+╔⊱ *${prefix}add*
+╠⊱ *${prefix}kick*
+╠⊱ *${prefix}opengc*
+╠⊱ *${prefix}closegc*
+╠⊱ *${prefix}linkgc*
+╠⊱ *${prefix}tagall*
+╠⊱ *${prefix}listadmins*
+╠⊱ *${prefix}welcome* [0/1]
+╠⊱ *${prefix}clone*
+╚⊱ *${prefix}simih* [0/1]
+
+*Others Commands*
+╔⊱ *${prefix}play* [judul]
+╠⊱ *${prefix}ytmp3* [link]
+╠⊱ *${prefix}ytsearch* [teks]
+╠⊱ *${prefix}chord* [judul]
+╠⊱ *${prefix}lirik* [judul]
+╠⊱ *${prefix}tiktok* [teks]
+╠⊱ *${prefix}tiktokstalk* [teks]
+╠⊱ *${prefix}url2img*
+╠⊱ *${prefix}blocklist*
+╠⊱ *${prefix}ocr* (reply with image)
+╠⊱ *${prefix}wait* (what is anime)
+║
+╠⊱ *${prefix}info* Untuk melihat Informasi Bot.
+╚═⊱ *Donasi?* *087898728747*`
+}
+
+exports.help = help
